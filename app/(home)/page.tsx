b62529381774f9de3c93b56b2d0347dd82f2177f
Phablo Carvalho <phablocarvalho14@hotmail.com>
2024-11-11 17:11:06 -0300

    feat: ensure get dashboard function is protected

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -20,7 +20,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
 
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("?month=01");
+    redirect(`?month=${new Date().getMonth() + 1}`);
   }
 
   const dashboard = await getDashboard(month);
@@ -28,15 +28,15 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr_1fr]">
-          <div className="flex flex-col gap-6">
+        <div className="grid h-full grid-cols-[2fr_1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards month={month} {...dashboard} />
-            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionPieChart {...dashboard} />
               <ExpensesPerCategory
                 expensesPerCategory={dashboard.totalExpensePerCategory}
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,6 +5,7 @@ import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   //acessar as transações do banco meu banco de dados
@@ -20,13 +21,15 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         {/* TÍTULO E BOTÃO */}
-        <div className="flex w-full items-center justify-between p-6">
+        <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable columns={TransactionColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={TransactionColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
   );
